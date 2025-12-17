bce74677bfe273f009b2dfcfcb9e20d90210de87
Luca M. <luca.demelo18@gmail.com>
2025-12-17 14:46:36 +0100

    LA C BON

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
-use App\Controller\HomeController;
-use App\Model\User;
-
 <?php
-// Importation de l'autoloader généré par Composer
-require_once 'vendor/autoload.php';
 
-$loader = new App\Helpers\Psr4AutoloaderClass();
+// Importation de l'autoloader généré par Composer
+require_once 'Helpers/Psr4AutoloaderClass.php';
+use Helpers\Psr4AutoloaderClass;
+
+$loader = new Psr4AutoloaderClass();
 $loader->register();
 
 $loader->addNamespace('\Helpers', '/Helpers');
-$loader->addNamespace('League\Plates', '/vendor/league/plates/src');
+$loader->addNamespace('\League\Plates', '/Vendor/Plates/src');
+
